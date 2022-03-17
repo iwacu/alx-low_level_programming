82f877baad19db274f698e500198b64ca7bd9942
iwacu <rutayisirewilly9@gmail.com>
2022-03-17 18:54:23 +0200

    c nested ..

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - Check for digits  (0 through 9)
+ * @c: int type param
+ * Return: 1 if digits, else 0
+ */
+
+int _isdigit(int c)
+{
+	if (c > 47 && c < 58)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
